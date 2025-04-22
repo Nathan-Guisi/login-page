50676cb54f2ff5d684bcaada653f86b9c7a6ac9d
Nathan <nathanguisi@gmail.com>
2025-04-22 19:57:29 -0300

    feat: create login-page

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { loginResponse } from '../components/types/login-response.type';
+import { tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  login(name: string, password: String){
+    return this.httpClient.post<loginResponse>("/login", {name, password}).pipe(
+      tap((value)=> {
+        sessionStorage.setItem("auth-token", value.token)
+        sessionStorage.setItem("username", value.name)
+      }
+    ))
+  }
+}
