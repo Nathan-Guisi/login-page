580e0ad8647beea4f953de0d2e2ddc441e6d7c46
Nathan <nathanguisi@gmail.com>
2025-04-22 21:00:16 -0300

    feat: create signup-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
-import { PagesLoginComponent } from './pages-login/pages-login.component';
+import { PagesLoginComponent } from './pages/login/pages-login.component';
+import { SignUpComponent } from './pages/signup/pages-signup.component';
 
 export const routes: Routes = [
     {
         path: "login",
         component: PagesLoginComponent
+    },
+
+    {
+        path: "signup",
+        component: SignUpComponent
     }
 ];
